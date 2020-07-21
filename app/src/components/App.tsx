b70064bbf139b42f9a480ae14b55ae6822a4168f
WKHAllen <wkhallen@gmail.com>
2020-07-21 18:19:25 -0400

    Add home and 404 components

--- app/src/components/App.tsx
+++ app/src/components/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import '../css/App.css';
+import NotFound from './errors/NotFound';
+import Home from './Home';
 import Test1 from './Test1';
 import Test2 from './Test2';
 import Test3 from './Test3';
@@ -25,9 +27,11 @@ export default class App extends React.Component {
 						</li>
 					</ul>
 					<Switch>
+						<Route exact path="/" component={Home}></Route>
 						<Route exact path="/test1" component={Test1}></Route>
 						<Route exact path="/test2" component={Test2}></Route>
 						<Route exact path="/test3" component={Test3}></Route>
+						<Route component={NotFound}></Route>
 					</Switch>
 				</div>
 			</Router>
