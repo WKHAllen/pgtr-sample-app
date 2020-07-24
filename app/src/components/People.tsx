da432efabe4b1b9024b1682d6208d56a639e002c
WKHAllen <wkhallen@gmail.com>
2020-07-23 23:51:59 -0400

    Build people and quotes pages

--- app/src/components/People.tsx
+++ app/src/components/People.tsx
@@ -1,10 +1,62 @@
 import React from 'react';
+import '../css/People.css';
 
-export default class People extends React.Component {
+interface PeopleState {
+	person: string
+}
+
+export default class People extends React.Component<{}, PeopleState> {
+	constructor(props: any) {
+		super(props);
+		this.state = {
+			person: ''
+		};
+	}
+
+	getJSON(url: string) {
+		return fetch(url).then(res => res.json());
+	}
+
+	getPersonById(event: React.ChangeEvent<HTMLInputElement>) {
+		this.getJSON(`/api/person/id/${event.target.value}`)
+			.then(console.log);
+	}
+
+	getRandomPerson() {
+		this.getJSON('/api/person/random')
+			.then(console.log);
+	}
+
+	getAllPeople() {
+		this.getJSON('/api/people')
+			.then(console.log);
+	}
+
 	render() {
 		return (
-			<div>
-				People
+			<div className="People">
+				<div className="form-group">
+					<label htmlFor="person-id">Person ID</label>
+					<input id="person-id"
+						   className="form-control"
+						   type="number"
+						   onChange={(event) => this.getPersonById(event)} />
+				</div>
+				<div className="form-group">
+					<button className="btn btn-primary"
+							type="button"
+							onClick={() => this.getRandomPerson()}>
+						Random person
+					</button>
+				</div>
+				<div className="form-group">
+					<button className="btn btn-primary"
+							type="button"
+							onClick={() => this.getAllPeople()}>
+						All people
+					</button>
+				</div>
+				<div>{this.state.person}</div>
 			</div>
 		);
 	}
--- app/src/components/Quotes.tsx
+++ app/src/components/Quotes.tsx
@@ -1,10 +1,62 @@
 import React from 'react';
+import '../css/Quotes.css';
 
-export default class Quotes extends React.Component {
+interface QuotesState {
+	quote: string
+}
+
+export default class Quotes extends React.Component<{}, QuotesState> {
+	constructor(props: any) {
+		super(props);
+		this.state = {
+			quote: ''
+		};
+	}
+
+	getJSON(url: string) {
+		return fetch(url).then(res => res.json());
+	}
+
+	getQuoteById(event: React.ChangeEvent<HTMLInputElement>) {
+		this.getJSON(`/api/quote/id/${event.target.value}`)
+			.then(console.log);
+	}
+
+	getRandomQuote() {
+		this.getJSON('/api/quote/random')
+			.then(console.log);
+	}
+
+	getAllQuotes() {
+		this.getJSON('/api/quotes')
+			.then(console.log);
+	}
+
 	render() {
 		return (
-			<div>
-				Quotes
+			<div className="Quotes">
+				<div className="form-group">
+					<label htmlFor="quote-id">Quote ID</label>
+					<input id="quote-id"
+						   className="form-control"
+						   type="number"
+						   onChange={(event) => this.getQuoteById(event)} />
+				</div>
+				<div className="form-group">
+					<button className="btn btn-primary"
+							type="button"
+							onClick={() => this.getRandomQuote()}>
+						Random quote
+					</button>
+				</div>
+				<div className="form-group">
+					<button className="btn btn-primary"
+							type="button"
+							onClick={() => this.getAllQuotes()}>
+						All quotes
+					</button>
+				</div>
+				<div>{this.state.quote}</div>
 			</div>
 		);
 	}
