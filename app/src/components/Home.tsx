abbd411c064b6bf40e810fa3690a28dee1525dae
WKHAllen <wkhallen@gmail.com>
2020-07-23 23:00:55 -0400

    Add navigation to home page

--- app/src/components/Home.tsx
+++ app/src/components/Home.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../css/Home.css';
 
 export default class Home extends React.Component {
 	render() {
 		return (
-			<ul className="App-header">
-				<li>
-					<Link to="/people">People</Link>
-				</li>
-				<li>
-					<Link to="/quotes">Quotes</Link>
-				</li>
-			</ul>
+			<div className="Home">
+				<h1>Navigation</h1>
+				<div className="Nav-Group">
+					<span><Link to="/people" className="btn btn-primary">People</Link></span>
+					<span><Link to="/quotes" className="btn btn-primary">Quotes</Link></span>
+				</div>
+			</div>
 		);
 	}
 }
