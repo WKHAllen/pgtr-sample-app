72e6a1f292177d22a4775d6e04ab184143ab6c25
WKHAllen <wkhallen@gmail.com>
2020-07-28 20:55:33 -0400

    Add more home page info

--- app/src/components/Info.tsx
+++ app/src/components/Info.tsx
@@ -6,11 +6,13 @@ export default class Info extends React.Component {
         return (
             <div className="Info">
                 This app shows off the capabilities of the PGTR stack.
-                It can be used as a template for more practical applications.
-                To use it, <a href="https://github.com/WKHAllen/pgtr-sample-app" target="_blank" rel="noopener noreferrer">see the source</a>.
-                Details on the stack and instructions on how to build upon the template application are provided.
                 Click the buttons above to see the app in use.
                 You can always navigate back home by clicking on the logo or text in the header.
+                The project can be used as a template for more practical applications.
+                To use it, <a href="https://github.com/WKHAllen/pgtr-sample-app" target="_blank" rel="noopener noreferrer">see the source</a>.
+                Details on the stack and instructions on how to build upon the template application are provided.
+                Stack details can be found <a href="https://github.com/WKHAllen/pgtr-sample-app#stack" target="_blank" rel="noopener noreferrer">here</a>.
+                Instructions on building the application can be found <a href="https://github.com/WKHAllen/pgtr-sample-app#setup" target="_blank" rel="noopener noreferrer">here</a>.
             </div>
         );
     }
